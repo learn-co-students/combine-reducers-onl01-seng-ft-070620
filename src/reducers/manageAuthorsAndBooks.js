524a3337644334af45e7776111f9ef9910e39319
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-11-18 18:26:09 -0500

    Done.

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,7 +1,7 @@
 export default function bookApp(
   state = {
     authors: [],
-    books: []
+    books: [],
   },
   action
 ) {
@@ -10,27 +10,34 @@ export default function bookApp(
     case "ADD_BOOK":
       return {
         ...state,
-        books: [...state.books, action.book]
+        authors: [...state.authors],
+        books: [...state.books, action.book],
       };
 
     case "REMOVE_BOOK":
-      idx = state.books.findIndex(book => book.id === action.id);
+      idx = state.books.findIndex((book) => book.id === action.id);
       return {
         ...state,
-        books: [...state.books.slice(0, idx), ...state.books.slice(idx + 1)]
+        authors: [...state.authors],
+        books: [...state.books.slice(0, idx), ...state.books.slice(idx + 1)],
       };
 
     case "ADD_AUTHOR":
       return {
         ...state,
-        authors: [...state.authors, action.author]
+        books: [...state.books],
+        authors: [...state.authors, action.author],
       };
 
     case "REMOVE_AUTHOR":
-      idx = state.authors.findIndex(author => author.id === action.id);
+      idx = state.authors.findIndex((author) => author.id === action.id);
       return {
         ...state,
-        authors: [...state.authors.slice(0, idx), ...state.authors.slice(idx + 1)]
+        books: [...state.books],
+        authors: [
+          ...state.authors.slice(0, idx),
+          ...state.authors.slice(idx + 1),
+        ],
       };
 
     default:
